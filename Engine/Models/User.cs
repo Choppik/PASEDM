756f194311bd59f7eba26ecff7fbeffeba035fa7
Егор Колосов <eg.kolosoff2016@yandex.ru>
2023-03-11 18:20:58 +0300

    Добавьте файлы проекта.

---
+++ Engine/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Engine/ViewModels/UserSession.cs
@@ -0,0 +1,21 @@
+﻿using Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.ViewModels
+{
+    public class UserSession
+    {
+        User CurrentUser { get; set; }
+        public UserSession()
+        {
+            CurrentUser = new User();
+            CurrentUser.IdUser = 1;
+            CurrentUser.Login = "User1";
+            CurrentUser.Password = "1234567890";
+        }
+    }
+}
