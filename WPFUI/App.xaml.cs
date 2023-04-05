7d4b0c8dca890efde4e2aea33342e5654537f166
Егор Колосов <eg.kolosoff2016@yandex.ru>
2023-04-05 20:00:29 +0300

    Внедрение зависимостей

--- WPFUI/App.xaml.cs
+++ WPFUI/App.xaml.cs
@@ -4,59 +4,80 @@ using PASEDM.Store;
 using PASEDM.View;
 using PASEDM.Services;
 using System;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace PASEDM
 {
     public partial class App : Application
     {
-        private NavigationStore _navigationStore;
-        private UserStore _userStore;
+        private readonly IServiceProvider _serviceProvider;
 
         public App()
         {
-            _navigationStore = new NavigationStore();
-            _userStore = new UserStore();
+            IServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<NavigationStore>();
+            services.AddSingleton<UserStore>();
+
+            services.AddSingleton<INavigationService>(s => CreateEntryUserNavigationService(s));
+
+            services.AddSingleton<MainWindowViewModel>();
+
+            services.AddSingleton<MainWindow>(s => new MainWindow()
+            {
+                DataContext = s.GetRequiredService<MainWindowViewModel>()
+            });
+
+            _serviceProvider = services.BuildServiceProvider();
+
         }
 
 
         protected override void OnStartup(StartupEventArgs e)
         {
 
-            INavigationService navigationService = CreateEntryUserNavigationService();
-            navigationService.Navigate();
+            INavigationService initialNavigationService = _serviceProvider.GetRequiredService<INavigationService>();
+            initialNavigationService.Navigate();
 
-            MainWindow = new MainWindow()
-            {
-                DataContext = new MainWindowViewModel(_navigationStore)
-            };
-
+            MainWindow = _serviceProvider.GetRequiredService<MainWindow>();
             MainWindow.Show();
 
             base.OnStartup(e);
         }
 
-        private INavigationService CreateMainMenuNavigationService()
+        private INavigationService CreateMainMenuNavigationService(IServiceProvider serviceProvider)
         {
-            return new LayoutNavigationService<MenuViewModel>(_navigationStore, () => 
-            new MenuViewModel(_userStore, CreateEntryUserNavigationService()), CreateNavigationBarViewModel);
+            UserStore userStore = serviceProvider.GetRequiredService<UserStore>();
+
+            return new LayoutNavigationService<MenuViewModel>(serviceProvider.GetRequiredService<NavigationStore>(), () => 
+            new MenuViewModel(userStore, 
+            CreateEntryUserNavigationService(serviceProvider)), () => CreateNavigationBarViewModel(serviceProvider));
         }
-        private INavigationService CreateUserNewNavigationService()
+        private INavigationService CreateUserNewNavigationService(IServiceProvider serviceProvider)
         {
-            return new NavigationService<UserGreatViewModel>(_navigationStore, () =>
-            new UserGreatViewModel(CreateEntryUserNavigationService()));
+            NavigationStore navigationStore = serviceProvider.GetRequiredService<NavigationStore>();
+
+            return new NavigationService<UserGreatViewModel>(navigationStore, () =>
+            new UserGreatViewModel(CreateEntryUserNavigationService(serviceProvider)));
         }
 
-        private INavigationService CreateEntryUserNavigationService()
+        private INavigationService CreateEntryUserNavigationService(IServiceProvider serviceProvider)
         {
-            return new NavigationService<UserEntryViewModel>(_navigationStore, () =>
-            new UserEntryViewModel(_userStore, CreateMainMenuNavigationService(), CreateUserNewNavigationService()));
+            UserStore userStore = serviceProvider.GetRequiredService<UserStore>();
+
+            return new NavigationService<UserEntryViewModel>
+                (serviceProvider.GetRequiredService<NavigationStore>(), 
+                () => new UserEntryViewModel(userStore, 
+                CreateMainMenuNavigationService(serviceProvider), CreateUserNewNavigationService(serviceProvider)));
         }
-        private NavigationBarViewModel CreateNavigationBarViewModel()
+        private NavigationBarViewModel CreateNavigationBarViewModel(IServiceProvider serviceProvider)
         {
-            return new NavigationBarViewModel(_userStore,
-                            CreateMainMenuNavigationService(),
-                            CreateUserNewNavigationService(), 
-                            CreateEntryUserNavigationService());
+            UserStore userStore = serviceProvider.GetRequiredService<UserStore>();
+
+            return new NavigationBarViewModel(userStore,
+                            CreateMainMenuNavigationService(serviceProvider),
+                            CreateUserNewNavigationService(serviceProvider), 
+                            CreateEntryUserNavigationService(serviceProvider));
         }
     }
 }
